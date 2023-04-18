d83809e65790a90f896a4a340fcc2e9719481abc
WasimSutar <waseemsutar@gmail.com>
2023-04-18 17:22:58 +0530

    master commit

---
+++ hello-world/hello_world.py
@@ -0,0 +1,13 @@
+
+
+def print_name(myname : str = ''):
+    print(myname)
+
+##############
+#### Main ####
+##############
+if __name__ == '__main__':
+    print('Hello world')
+    print('Print my name')
+    myname = 'Chethan'
+    print_name(f'My name is : {myname}')
